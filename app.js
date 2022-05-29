a485768a1803f56c9d5ca2d4271ef48f6802e2a8
Christopher Hicks <visrule@protonmail.com>
2022-05-29 11:16:37 -0500

    setup router and index

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// require in all routers
+const router = require("./routes/router");
+
 // set public files - CSS etc.
 app.use(express.static("public"));
 // first arg - boiler; 2nd arg - folder location for views
@@ -8,8 +11,7 @@ app.set("views", "views");
 // set view engine 1st arg - boiler; 2nd arg - engine - npm install ejs
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("home-guest");
-});
+// set routers
+app.use(router);
 
 app.listen(3000);
